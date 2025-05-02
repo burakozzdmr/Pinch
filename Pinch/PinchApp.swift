cafa51930acaddeb614b0a52362958ad45b66aba
Burak Özdemir <mburak.ozdemir03@gmail.com>
2025-05-02 11:16:07 +0300

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Burak Özdemir on 2.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
