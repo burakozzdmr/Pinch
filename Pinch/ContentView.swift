47fcffed26f12e4c4257062fb04686f8a0aecd42
Burak Özdemir <mburak.ozdemir03@gmail.com>
2025-05-02 23:53:31 +0300

    Add: homepage screen ui design completed

--- Pinch/ContentView.swift
+++ Pinch/ContentView.swift
@@ -8,14 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    // MARK: - PROPERTIES
+    
+    @State private var isAnimating: Bool = false
+    @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
+    
+    // MARK: - FUNCTIONS
+    
+    func resetImageState() {
+        return withAnimation(.spring()) {
+            imageScale = 1
+            imageOffset = .zero
         }
-        .padding()
+    }
+    
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(12)
+                    .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .padding()
+                    .opacity(isAnimating ? 1 : 0)
+                    .animation(.linear(duration: 1), value: isAnimating)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
+                    .scaleEffect(imageScale)
+                    .onTapGesture(count: 2) {
+                        if imageScale == 1 {
+                            withAnimation(.spring()) {
+                                imageScale = 5
+                            }
+                        } else {
+                            resetImageState()
+                        }
+                    }
+                    .gesture(
+                        DragGesture()
+                            .onChanged({ value in
+                                withAnimation(.linear(duration: 1)) {
+                                    imageOffset = value.translation
+                                }
+                            })
+                            .onEnded({ _ in
+                                if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                            })
+                    )
+            } //: ZSTACK
+            .navigationTitle("Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear {
+                isAnimating = true
+            }
+        } //: NAVIGATION VIEW
+        .navigationViewStyle(.stack)
     }
 }
 
